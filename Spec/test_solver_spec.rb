4b18e559ab65ee2310f3fba70a2e806d62fca950
ibugithub <outsideworkibrahim@gmail.com>
2023-08-10 22:37:57 +0600

    Add the test case for negative argument

--- Spec/test_solver_spec.rb
+++ Spec/test_solver_spec.rb
@@ -6,5 +6,20 @@ describe Solver do
             solver = Solver.new
             expect(solver.factorial(5)).to eq(120)
         end
+
+        it "Should return 1 for the argument of 0" do 
+            solver = Solver.new
+            expect(solver.factorial(0)).to eq(1)
+        end 
+
+        it "Should return 1 for the argument of 1" do 
+            solver = Solver.new
+            expect(solver.factorial(1)).to eq(1)
+        end 
+
+        it "Should raise an exception for the argument of -1" do 
+            solver = Solver.new
+            expect { solver.factorial(-1) }.to raise_error(ArgumentError, "Negative won't get accepted")
+        end 
     end
 end
