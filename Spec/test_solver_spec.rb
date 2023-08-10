e834cc02b2a43d0c2b4b1c0cc25cdf8100389749
jic <ji_camargo@yahoo.com>
2023-08-10 12:18:38 -0500

    create test unit for reverse method

--- Spec/test_solver_spec.rb
+++ Spec/test_solver_spec.rb
@@ -1,25 +1,35 @@
 require './solver'
 
+# Test unit for factorial method
 describe Solver do 
-    context "check the factorial method" do 
-        it"Should return 120 for the argument of 5 " do
-            solver = Solver.new
-            expect(solver.factorial(5)).to eq(120)
-        end
-
-        it "Should return 1 for the argument of 0" do 
-            solver = Solver.new
-            expect(solver.factorial(0)).to eq(1)
-        end 
-
-        it "Should return 1 for the argument of 1" do 
-            solver = Solver.new
-            expect(solver.factorial(1)).to eq(1)
-        end 
-
-        it "Should raise an exception for the argument of -1" do 
-            solver = Solver.new
-            expect { solver.factorial(-1) }.to raise_error(ArgumentError, "Negative won't get accepted")
-        end 
+  context "check the factorial method" do 
+    it"Should return 120 for the argument of 5 " do
+      solver = Solver.new
+      expect(solver.factorial(5)).to eq(120)
     end
+
+    it "Should return 1 for the argument of 0" do 
+      solver = Solver.new
+      expect(solver.factorial(0)).to eq(1)
+    end 
+
+    it "Should return 1 for the argument of 1" do 
+      solver = Solver.new
+      expect(solver.factorial(1)).to eq(1)
+    end 
+
+    it "Should raise an exception for the argument of -1" do 
+      solver = Solver.new
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError, "Negative won't get accepted")
+    end 
+  end
+end
+
+# Test unit for reverse method
+describe "reverse method" do
+  it 'reverses a word' do
+    solver = Solver.new
+    result = solver.reverse('hello')
+    expect(result).to eq('olleh')
+   end
 end
