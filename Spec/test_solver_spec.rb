cc4923088f26b514cac492bbaf630838af6e3c26
ibugithub <outsideworkibrahim@gmail.com>
2023-08-10 21:44:31 +0600

    Write the first test for the factorial method

--- Spec/test_solver_spec.rb
+++ Spec/test_solver_spec.rb
@@ -1,5 +1,10 @@
 require './solver'
 
 describe Solver do 
-
+    context "check the factorial method" do 
+        it"Should return 120 for the argument of 5 " do
+            solver = Solver.new
+            expect(solver.factorial(5)).to eq(120)
+        end
+    end
 end
