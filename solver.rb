640188d7eecb7a3843d40b8a5a0350c51c56f8d1
jic <ji_camargo@yahoo.com>
2023-08-10 12:35:11 -0500

    fix fizzbuzz method: add case for N is divisible for 3 and 5

--- solver.rb
+++ solver.rb
@@ -13,7 +13,9 @@ class Solver
   end
 
   def fizzbuzz(n)
-    if n % 3 == 0
+    if n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    elsif n % 3 == 0
       'fizz'
     elsif n % 5 == 0
       'buzz'
