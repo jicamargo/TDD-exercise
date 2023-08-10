70b6f2c0fd65d3d7e67be1f4e1f35f52b1c65f9c
jic <ji_camargo@yahoo.com>
2023-08-10 12:27:10 -0500

    add method fizzbuzz to solver class

--- solver.rb
+++ solver.rb
@@ -1,14 +1,22 @@
 class Solver 
-    def factorial(n)
-        if n == 1 || n == 0
-            return 1
-        elsif n < 0
-            raise ArgumentError, "Negative won't get accepted"
-        end
-        n * factorial(n-1)
+  def factorial(n)
+    if n == 1 || n == 0
+        return 1
+    elsif n < 0
+        raise ArgumentError, "Negative won't get accepted"
     end
+    n * factorial(n-1)
+  end
 
-    def reverse(word)
-        word.reverse
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(n)
+    if n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
     end
+  end
 end
